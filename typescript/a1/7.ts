3c6701a4e6487fbf1a6aa3e4b8bc9f7469d53e7c
chandu <chandumalireddy2410@gmail.com>
2021-03-31 15:29:03 +0530

    added

---
+++ typescript/a1/7.ts
@@ -0,0 +1,12 @@
+let array = [1,2,3];
+let [ , , third  ] = array;
+console.log(third);
+
+
+const a = {
+    "name":"chandu",
+    "address":"hyd",
+    "pincode":12121
+};
+let { , , pincode} = a;
+console.log(pincode);
