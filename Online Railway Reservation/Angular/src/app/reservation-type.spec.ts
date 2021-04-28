136fef5ddbdd75bc07d65a2a13ffd2d5c1630cf4
chandumalireddy <63465345+chandumalireddy@users.noreply.github.com>
2021-04-28 14:12:50 +0530

    Delete Online Railway Reservation directory

--- Online Railway Reservation/Angular/src/app/reservation-type.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ReservationType } from './reservation-type';
-
-describe('ReservationType', () => {
-  it('should create an instance', () => {
-    expect(new ReservationType()).toBeTruthy();
-  });
-});
